985cb87a4833fa16b3589d806f37632e3bb8f4c7
Alejandra Diaz <ygthar@gmail.com>
2017-02-21 00:18:54 -0300

    seeds.rb changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Category.destroy_all
+
+3.times do |c|
+	d = Category.create(name: "category #{c}")
+	d.products.build(name: "product #{c}", price: 1000*c).save
+	Product.create(name: "product #{c}", price: 1000*c, category_id: d.id)
+end
+
+Product.last.destroy
+p = Product.last
+p.premium = true
+p.save
+
+d = Category.all
+d.each do |c|
+	c.name.upcase!
+	c.save
+end

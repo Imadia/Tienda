aa1b266a2cd3e16386b934987b3c077c2685a898
Alejandra Diaz <ygthar@gmail.com>
2017-02-20 23:21:37 -0300

    Product. Default, premium false

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :category
+  before_validation :set_premium
+  validates :price, presence: true
+
+  def set_premium
+  	unless premium.present?
+  		self.premium = false
+  	end
+	end
 end

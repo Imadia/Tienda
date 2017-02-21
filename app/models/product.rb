f1d5a3b47f24a642bf2dc553daf1f7670539a9b1
Alejandra Diaz <ygthar@gmail.com>
2017-02-21 00:34:00 -0300

    scope added

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category
   before_validation :set_premium
   validates :price, presence: true
+  scope :premium, -> { where(premium: true)}
+  scope :last_n, -> (n) { order('updated_at desc').limit(n)}
 
   def set_premium
   	unless premium.present?
